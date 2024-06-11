b7191a91e1fb0c7b5d1771574f34b3432f26472d
Mashiro <165439327+MashiroIsAsleep@users.noreply.github.com>
2024-06-10 20:18:25 -0400

    Add files via upload

---
+++ JavaSnake/src/SnakeGame.java
@@ -0,0 +1,11 @@
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        createGameFrame();
+    }
+
+    private static void createGameFrame() {
+        @SuppressWarnings("unused")
+        GameFrame gameFrame = new GameFrame();
+    }
+}
