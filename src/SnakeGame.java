10342c917e1fbe5595443b6da420ed7ebbcbc6a6
Mashiro <165439327+MashiroIsAsleep@users.noreply.github.com>
2024-06-10 20:43:06 -0400

    changed SnakeGame.java

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,11 +1,13 @@
 public class SnakeGame {
 
     public static void main(String[] args) {
+        //Call createGameFrame function to run game
         createGameFrame();
     }
 
+    //Function to run the game frame
     private static void createGameFrame() {
-        @SuppressWarnings("unused")
-        GameFrame gameFrame = new GameFrame();
+        new GameFrame();
     }
 }
+
